f0baffac8763fa8c3d87ebb4be72b459c24f1c73
serec <serec@ceres-Aspire-E5>
2019-10-09 18:17:39 -0700

    Initial version

---
+++ As2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,59 @@
+package cse360assign2;
+
+/**
+ * This class allows for addition and subtraction of numbers. In
+ * addition it keeps a record of additions and subtractions and keeps
+ * track of the total.
+ * @author Ceres Perez
+ */
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * Constructor starts with total at zero. No arguments
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the total of the adding machine
+	 * @returns total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Add the argument to the total and keeps a record of the addition
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts the argument from the total and keeps a record of
+	 * the subtraction
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Returns a string containing the record of additions and transactions
+	 * @returns record
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	/**
+	 * Resets the total to zero and clears the record
+	 */
+	public void clear() {
+	
+	}
+}
